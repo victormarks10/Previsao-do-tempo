7018569c6f39b54d87046832ece9a4738ed47f21
Victormarks10 <aaron.victormarks10@gmail.com>
2023-02-08 19:16:22 -0300

    projeto-clima-tempo

---
+++ script.js
@@ -0,0 +1,34 @@
+let chave = "0989957ba645103620152c3af89e0788"
+
+function colocarNatela(dados) {
+  console.log(dados)
+
+  document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+  document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
+  document.querySelector(".descricao").innerHTML = dados.weather[0].description
+  document.querySelector(".icone").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
+  document.querySelector(".umidade").innerHTML = "Umidade : " + (dados.main.humidity) + "%"
+
+}
+
+
+   async function buscarCidade(cidade) {
+   let dados = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cidade + "&appid=" + chave + "&lang=pt_br"
+   + "&units=metric")
+
+  .then(resposta => resposta.json())
+   colocarNatela(dados)
+
+}
+
+
+
+function cliqueiNoBotao() {
+
+  let cidade = document.querySelector(".input-cidade").value
+
+  buscarCidade(cidade)
+
+
+
+}
